8ede66694023d2dd35c5ab48ac9ba547d5103e8b
Daiki Saito <Daiki.Saito@isobar.com>
2018-10-03 15:32:00 +0900

    link test

--- nuxt.config.js
+++ nuxt.config.js
@@ -13,6 +13,9 @@ module.exports = {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  router: {
+    base: '/app/'
+},
   /*
   ** Customize the progress bar color
   */
--- pages/one.vue
+++ pages/one.vue
@@ -1,26 +1,8 @@
 <template>
   <section class="container">
     <div>
-      <app-logo/>
-      <h1 class="title">
-        vue-dev02-nuxt
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <!-- <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a> -->
-
-          <router-link class="button--green" to="one">one</router-link>
-
-      </div>
+        <h1 class="title">page one</h1>
+        <p><router-link class="button--green" to="/">Back</router-link></p>
     </div>
   </section>
 </template>
