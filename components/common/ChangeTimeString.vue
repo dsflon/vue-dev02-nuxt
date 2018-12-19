5bd6b1ac2123c6d117b1b90b2cbc53636bd00b6c
Daiki Saito <Daiki.Saito@isobar.com>
2018-12-19 12:24:35 +0900

    changeTimeString.vue 実装

---
+++ components/common/ChangeTimeString.vue
@@ -0,0 +1,67 @@
+<!-- const ChangeTimeString = (n) => {
+    if(!n) return null;
+
+    let _n = Number(n);
+        _n = _n > 2400 ? (_n - 2400) : _n;
+
+    let t = String(_n).split("");
+
+    if(_n < 1000) {
+        t = t[0] + ":" + t[1] + t[2];
+        return n > 2400 ? (`<span><em class="a-txt_s">翌 </em>${t}</span>`) : t;
+    } else {
+        return t[0] + t[1] + ":" + t[2] + t[3];
+    }
+
+} -->
+
+
+<template>
+
+    <span v-if=" GetTimeNum(num)._n < 1000 && GetTimeNum(num).n > 2400 ">
+        <em class="a-txt_s">翌 </em>{{ GetTimeNum(num).t }}
+    </span>
+
+    <span v-else-if=" GetTimeNum(num)._n < 1000 && GetTimeNum(num).n <= 2400 ">
+        {{ GetTimeNum(num).t }}
+    </span>
+
+    <span v-else>
+        {{ GetTimeNum(num).t }}
+    </span>
+
+</template>
+
+<script>
+export default {
+    props: [
+        "num"
+    ],
+    computed: {
+        GetTimeNum() {
+            return (n) => {
+
+                if(!n) return null;
+
+                let _n = Number(n);
+                    _n = _n > 2400 ? (_n - 2400) : _n;
+
+                let t = String(_n).split("");
+
+                if(_n < 1000) {
+                    t = t[0] + ":" + t[1] + t[2];
+                } else {
+                    t = t[0] + t[1] + ":" + t[2] + t[3];
+                }
+
+                return {
+                    n: n,
+                    _n: _n,
+                    t: t
+                }
+            }
+        }
+    }
+
+}
+</script>
--- components/home/ListItems.vue
+++ components/home/ListItems.vue
@@ -3,16 +3,16 @@
         <ul class="lists_inner">
 
             <li class="list"
-                v-for="d in this.$store.state.home.searchResult"
-                :key="d.user_name">
-                    <button id={d.user_id} class="list_btn" onClick={this.props.GetPage.bind(this)}>
+                v-for="item in this.$store.state.home.searchResult"
+                :key="item.user_name">
+                    <button id={item.user_id} class="list_btn" onClick={this.props.GetPage.bind(this)}>
 
                         <div
                             class="list_user"
-                            data-src={d.user_icon}>
+                            data-src={item.user_icon}>
                             <div class="list_user_wrap">
-                                <p class="list_user_access">{{d.access_info}}</p>
-                                <p class="list_user_name">{{d.user_name}}</p>
+                                <p class="list_user_access">{{item.access_info}}</p>
+                                <p class="list_user_name">{{item.user_name}}</p>
                             </div>
                             <!-- {{delayLoader}} -->
                         </div>
@@ -20,25 +20,27 @@
                             <ul class="m-info_counts is_flex is_s">
                                 <li>
                                     <i class="a-icon a-icon-friends is_gray"></i>
-                                    <span class="a-icon_txt">{{d.count_follows.toLocaleString()}}</span>
+                                    <span class="a-icon_txt">{{item.count_follows.toLocaleString()}}</span>
                                 </li>
                                 <li>
                                     <i class="a-icon a-icon-heart is_gray"></i>
-                                    <span class="a-icon_txt">{{d.count_likes.toLocaleString()}}</span>
+                                    <span class="a-icon_txt">{{item.count_likes.toLocaleString()}}</span>
                                 </li>
                                 <li>
                                     <i class="a-icon a-icon-picture is_gray"></i>
-                                    <span class="a-icon_txt">{{d.count_posts.toLocaleString()}}</span>
+                                    <span class="a-icon_txt">{{item.count_posts.toLocaleString()}}</span>
                                 </li>
                             </ul>
                             <div class="list_info_wrap">
-                                <p class="list_info_time">
+                                <div class="list_info_time">
                                     <i class="a-icon a-icon-time"></i>
-                                    <!-- <span class="a-icon_txt">{{ChangeTimeString(d.start_time)}} ~ {{ChangeTimeString(d.end_time)}}</span> -->
-                                </p>
-                                <p class="list_info_price">
-                                    ¥ {{d.min_price.toLocaleString()}} ~ {{d.max_price.toLocaleString()}}
-                                </p>
+                                    <div class="a-icon_txt">
+                                        <change-time-string :num="item.start_time" /> ~ <change-time-string :num="item.end_time" />
+                                    </div>
+                                </div>
+                                <div class="list_info_price">
+                                    ¥ {{item.min_price.toLocaleString()}} ~ {{item.max_price.toLocaleString()}}
+                                </div>
                             </div>
                         </div>
                     </button>
@@ -51,15 +53,11 @@
 
 <script>
 
+import ChangeTimeString from '~/components/common/ChangeTimeString.vue'
+
 export default {
-    // data () {
-    //     return {
-    //     }
-    // },
-    mounted: function() {
-    },
-    updated: function() {
-        console.log(this.$store.state.home.searchResult);
+    components: {
+        ChangeTimeString
     }
 }
 </script>
