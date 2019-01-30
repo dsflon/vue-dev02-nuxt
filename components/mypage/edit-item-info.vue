61b77e37beba312c2c50d604157148592f4bff6b
Daiki Saito <Daiki.Saito@isobar.com>
2019-01-30 13:48:40 +0900

    プロフィール編集画面 カテゴリ追加機能

--- components/mypage/edit-item-info.vue
+++ components/mypage/edit-item-info.vue
@@ -6,57 +6,34 @@
             v-for="(item,i) in infoData"
             :key="i" class="info_box">
 
-            <div v-if="item.category==='store'">
-                <div class="info_box_ttl">
-                    <h2>{{item.title}}</h2>
-                    <button type="button" class="info_box_menu"><i class="a-icon a-icon-menu a-icon-lg"></i></button>
-                </div>
-                <edit-item-info-store
-                    :i="i"
-                    :item="item"
-                    :OnFocus="OnFocus"
-                    :OnBlur="OnBlur" />
+            <div class="info_box_ttl">
+                <label class="m-form_bg m-form_label">
+                    <input
+                        class="a-form_input is_bold"
+                        type="text"
+                        placeholder="カテゴリ名を入力してください"
+                        @focus="OnFocus"
+                        @blur="OnBlur"
+                        v-model="item.title">
+                </label>
+                <button type="button" class="info_box_menu is_black">
+                    <i class="a-icon a-icon-menu a-icon-lg"></i>
+                </button>
             </div>
+
+            <edit-item-info-store
+                v-if="item.category==='store'"
+                :i="i" :item="item" :OnFocus="OnFocus" :OnBlur="OnBlur" />
             <!-- / item.type==='store' -->
 
-            <div v-else-if="item.category==='career'">
-                <div class="info_box_ttl">
-                    <h2>{{item.title}}</h2>
-                    <button type="button" class="info_box_menu"><i class="a-icon a-icon-menu a-icon-lg"></i></button>
-                </div>
-                <edit-item-info-career
-                    :i="i"
-                    :item="item"
-                    :OnFocus="OnFocus"
-                    :OnBlur="OnBlur" />
-            </div>
+            <edit-item-info-career
+                v-else-if="item.category==='career'"
+                :i="i" :item="item" :OnFocus="OnFocus" :OnBlur="OnBlur" />
             <!-- / item.type==='career' -->
 
-            <div v-else>
-                <div class="info_box_ttl">
-                    <label v-if="item.category==='other'" class="m-form_bg m-form_label">
-                        <input
-                            class="a-form_input is_bold"
-                            type="text"
-                            :name="'group_'+i"
-                            placeholder="カテゴリー名を入力してください"
-                            @focus="OnFocus"
-                            @blur="OnBlur"
-                            v-model="item.title">
-                    </label>
-                    <h2 v-else>{{item.title}}</h2>
-                    <button type="button" class="info_box_menu is_black">
-                        <i class="a-icon a-icon-menu a-icon-lg"></i>
-                    </button>
-                </div>
-
-                <edit-item-info-other
-                    :i="i"
-                    :item="item"
-                    :OnFocus="OnFocus"
-                    :OnBlur="OnBlur" />
-
-            </div>
+            <edit-item-info-other
+                v-else
+                :i="i" :item="item" :OnFocus="OnFocus" :OnBlur="OnBlur" />
 
         </section>
 
@@ -68,7 +45,7 @@
                 <div v-if="addItemWindow" class="info_box_btn_menu">
                     <ul>
                         <li><button type="button" @click="AddItem('store')"><i class="a-icon a-icon-plus is_white"></i><span class="a-icon_txt">お店</span></button></li>
-                        <li><button type="button" @click="AddItem('careea')"><i class="a-icon a-icon-plus is_white"></i><span class="a-icon_txt">経歴</span></button></li>
+                        <li><button type="button" @click="AddItem('career')"><i class="a-icon a-icon-plus is_white"></i><span class="a-icon_txt">経歴</span></button></li>
                         <li><button type="button" @click="AddItem('other')"><i class="a-icon a-icon-plus is_white"></i><span class="a-icon_txt">自由項目</span></button></li>
                     </ul>
                 </div>
@@ -102,7 +79,30 @@ export default {
     },
     data () {
         return {
-            addItemWindow: false
+            addItemWindow: false,
+            newItem: {
+                "store": {
+                    "title": "お店",
+                    "category": "store",
+                    "contents_": {
+                        "store_name": { "text": "" },
+                        "store_address": { "postal": "", "station": "", "gate": "", "transportation": "", "time": "", "text": ""},
+                        "store_time": { "start_time": "", "end_time": "" },
+                        "store_holiday": { "text": "" },
+                        "store_url": { "text": "" }
+                    }
+                },
+                "career": {
+                    "title": "経歴",
+                    "category": "career",
+                    "contents_": [ { "date": { "start": "", "end": "" }, "text": "" } ]
+                },
+                "other": {
+                    "title": "その他",
+                    "category": "other",
+                    "contents": [ { "title": "", "text": "" } ]
+                }
+            }
         }
     },
     computed : {
@@ -112,32 +112,8 @@ export default {
             this.addItemWindow = !this.addItemWindow;
         },
         AddItem: function(category) {
-            let newItem = switch (category) {
-                case "store":
-                return {
-                    "title": "",
-                    "category": category,
-                    "contents": [ { "title": "", "text": "" } ]
-                }
-
-                case "career":
-                return {
-                    "title": "",
-                    "category": "career",
-                    "contents": [ { "date": { "start": "", "end": "" }, "text": "" } ]
-                }
-
-                default:
-                return {
-                    "title": "",
-                    "category": category,
-                    "contents": [ { "title": "", "text": "" } ]
-                }
-
-            };
-
-
-            this.infoData.push(newItem)
+            this.infoData.push(this.newItem[category])
+            this.addItemWindow = false;
         }
     },
     mounted: function() {
